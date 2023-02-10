8f098b0bb851a1700656a214e483adaef3e14b3a
ryurukov2 <radoslav.vy@gmail.com>
2023-02-10 19:31:44 +0200

    test

--- ExpenseTracker/auth_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from ExpenseTracker.auth_app.views import SignUpView, SignInView, SignOutView
-
-urlpatterns = [
-    path('sign-up/', SignUpView.as_view(), name='sign up'),
-    path('sign-in/', SignInView.as_view(), name='login'),
-    path('sign-out/', SignOutView.as_view(), name='sign out'),
-]
