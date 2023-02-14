682348b70d40cf232baca0419acdf90ebdccc332
ryurukov2 <radoslav.vy@gmail.com>
2023-02-14 21:42:48 +0200

    working parser to add new csv files and remove dup

--- ExpenseTracker/parser/views.py
+++ ExpenseTracker/parser/views.py
@@ -23,36 +23,51 @@ def import_card_csv(request):
             # skip the header row
             next(csv_reader)
             txnowner = Profile.objects.get(user_id=request.user.id)
+            # print(txn_str)
+            existing_txns = set([
+                ', '.join(str(getattr(field, name).strftime('%Y-%m-%d %H:%M:%S')) if name == 'timestamp' else str(getattr(field, name)) if getattr(field, name) is not None else ''
+                          for name in ('timestamp', 'description', 'currency', 'amount', 'to_currency', 'to_amount', 'native_currency', 'native_amount')
+                          ) for field in Transaction.objects.filter(owner=txnowner)
+            ])
+
             # parse and save the data from the CSV file
             for row in csv_reader:
-                # parse the data from the row
-                date = row[0]
-                date_in_datetime = datetime.datetime.strptime(
-                    date, '%Y-%m-%d %H:%M:%S')
-                aware_datetime = pytz.timezone('UTC')
-                timestamp = aware_datetime.localize(date_in_datetime)
-                description = row[1]
-                currency = row[2]
-                amount = float(row[3])
-                to_currency = row[4] if row[4] else None
-                to_amount = float(row[5]) if row[5] else None
-                native_currency = row[6]
-                native_amount = float(row[7])
-                native_amount_usd = float(row[8])
+                row[3]="%.13f"%float(row[3])
+                row[5]="%.13f"%float(row[5]) if row[5] else row[5]
+                row[7]="%.13f"%float(row[7])
+                row[8]="%.13f"%float(row[8])
+                txn_data = ', '.join(row[:8])
+                if txn_data in existing_txns:
+                    continue
+                else:
+                    # parse the data from the row
+                    date = row[0]
+                    date_in_datetime = datetime.datetime.strptime(
+                        date, '%Y-%m-%d %H:%M:%S')
+                    aware_datetime = pytz.timezone('UTC')
+                    timestamp = aware_datetime.localize(date_in_datetime)
+                    description = row[1]
+                    currency = row[2]
+                    amount = float(row[3])
+                    to_currency = row[4] if row[4] else None
+                    to_amount = float(row[5]) if row[5] else None
+                    native_currency = row[6]
+                    native_amount = float(row[7])
+                    native_amount_usd = float(row[8])
 
-                # create a new Transaction object and save it to the database
-                Transaction.objects.create(
-                    owner=txnowner,
-                    timestamp=timestamp,
-                    description=description,
-                    currency=currency,
-                    amount=amount,
-                    to_currency=to_currency,
-                    to_amount=to_amount,
-                    native_currency=native_currency,
-                    native_amount=native_amount,
-                    native_amount_usd=native_amount_usd
-                )
+                    # create a new Transaction object and save it to the database
+                    Transaction.objects.create(
+                        owner=txnowner,
+                        timestamp=timestamp,
+                        description=description,
+                        currency=currency,
+                        amount=amount,
+                        to_currency=to_currency,
+                        to_amount=to_amount,
+                        native_currency=native_currency,
+                        native_amount=native_amount,
+                        native_amount_usd=native_amount_usd
+                    )
             return redirect('transactions')
     else:
         form = CsvImportForm()
@@ -64,6 +79,13 @@ def import_app_csv(request):
         form = CsvImportForm(request.POST, request.FILES)
         if form.is_valid():
             # get the uploaded file
+            txnowner = Profile.objects.get(user_id=request.user.id)
+            existing_txns = set([
+                ', '.join(str(getattr(field, name).strftime('%Y-%m-%d %H:%M:%S')) if name == 'timestamp' else str(getattr(field, name)) if getattr(field, name) is not None else ''
+                          for name in ('timestamp', 'description', 'currency', 'amount', 'to_currency', 'to_amount', 'native_currency', 'native_amount', 'transaction_kind', 'transaction_hash')
+                          ) for field in AppTransaction.objects.filter(owner=txnowner)
+            ])
+
             csv_file = request.FILES['csv_file']
 
             # open the file in Unicode-compatible mode
@@ -71,40 +93,52 @@ def import_app_csv(request):
 
             # skip the header row
             next(csv_reader)
-            txnowner = Profile.objects.get(user_id=request.user.id)
+            
             for row in csv_reader:
-                # parse the data from the row
-                date = row[0]
-                date_in_datetime = datetime.datetime.strptime(
-                    date, '%Y-%m-%d %H:%M:%S')
-                aware_datetime = pytz.timezone('UTC')
-                timestamp = aware_datetime.localize(date_in_datetime)
-                description = row[1]
-                currency = row[2]
-                amount = float(row[3])
-                to_currency = row[4] if row[4] else None
-                to_amount = float(row[5]) if row[5] else None
-                native_currency = row[6]
-                native_amount = float(row[7])
-                native_amount_usd = float(row[8])
-                transaction_kind = row[9]
-                transaction_hash = row[10] if row[10] else None
+                row[3]="%.13f"%float(row[3])
+                row[5]="%.13f"%float(row[5]) if row[5] else row[5]
+                row[7]="%.13f"%float(row[7])
+                row[8]="%.13f"%float(row[8])
+                print(row[2])
+                txn_data = ', '.join(row[:8]+row[9:])
+                
+                # print(txn_data)
+                
+                if txn_data in existing_txns:
+                    continue
+                else:
+                    # parse the data from the row
+                    date = row[0]
+                    date_in_datetime = datetime.datetime.strptime(
+                        date, '%Y-%m-%d %H:%M:%S')
+                    aware_datetime = pytz.timezone('UTC')
+                    timestamp = aware_datetime.localize(date_in_datetime)
+                    description = row[1]
+                    currency = row[2]
+                    amount = float(row[3])
+                    to_currency = row[4] if row[4] else None
+                    to_amount = float(row[5]) if row[5] else None
+                    native_currency = row[6]
+                    native_amount = float(row[7])
+                    native_amount_usd = float(row[8])
+                    transaction_kind = row[9]
+                    transaction_hash = row[10] if row[10] else None
 
-                # create a new AppTransaction object and save it to the database
-                AppTransaction.objects.create(
-                    owner=txnowner,
-                    timestamp=timestamp,
-                    description=description,
-                    currency=currency,
-                    amount=amount,
-                    to_currency=to_currency,
-                    to_amount=to_amount,
-                    native_currency=native_currency,
-                    native_amount=native_amount,
-                    native_amount_usd=native_amount_usd,
-                    transaction_kind=transaction_kind,
-                    transaction_hash=transaction_hash,
-                )
+                    # create a new AppTransaction object and save it to the database
+                    AppTransaction.objects.create(
+                        owner=txnowner,
+                        timestamp=timestamp,
+                        description=description,
+                        currency=currency,
+                        amount=amount,
+                        to_currency=to_currency,
+                        to_amount=to_amount,
+                        native_currency=native_currency,
+                        native_amount=native_amount,
+                        native_amount_usd=native_amount_usd,
+                        transaction_kind=transaction_kind,
+                        transaction_hash=transaction_hash,
+                    )
             return redirect('transactions')
     else:
         form = CsvImportForm()
@@ -151,7 +185,7 @@ def analytics(request):
     # render the template with the total_spent_by_month data
     if total_cashback_cro is not None and total_rebates_cro is not None and total_stake_cro is not None:
         total_overall_cro = total_cashback_cro + total_rebates_cro + total_stake_cro
-    
+
     context = {'total_spent_by_month': total_spent_by_month,
                'top_merchants': top_merchants,
                'total_spent': total_sp,
